ec9c52ce72b022d50d67d4fd67eadc44397d7cd5
Vishal-m <vishalmahesh.study@gmail.com>
2024-03-18 22:00:38 +0530

    functionalites done

--- todo-contextapi/src/App.jsx
+++ todo-contextapi/src/App.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { TodoProvider } from "./store";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [todo, setTodo] = useState([]);
 
-export default App
+  const addTodo = (newTodo) => {
+    setTodo((prev) => [{ id: new Date.now(), ...newTodo }, ...prev]);
+  };
+
+  const updateTodo = (updatedTodo, id) => {
+    setTodo((prev) =>
+      prev.map((prevTodo) => (prevTodo.id === id ? updateTodo : prevTodo))
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodo((prev) => todo.filter((prevTodo) => prevTodo.id !== id));
+  };
+
+  const toggleComplete = (id) => {
+    setTodo((prev) =>
+      prev.map((prevTodo) =>
+        prevTodo.id === id
+          ? { ...prevTodo, completed: !prevTodo.completed }
+          : prevTodo
+      )
+    );
+  };
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"));
+    if (todos && todos.length > 0) {
+      setTodo(todos);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todo));
+  }, [todo]);
+
+  return (
+    <TodoProvider
+      value={{ todo, addTodo, updateTodo, deleteTodo, toggleComplete }}
+    >
+      <div className="">hey</div>
+    </TodoProvider>
+  );
+};
+
+export default App;
